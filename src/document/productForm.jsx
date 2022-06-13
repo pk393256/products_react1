3e6eb8025b69622fa189e248de5708bdb8be160e
pk393256 <pk393256@gmail.com>
2022-06-13 22:37:16 +0530

    check2

---
+++ src/document/productForm.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+function ProductForm() {
+    const [data, setData] = React.useState({
+        id:'',
+        title:'',
+        gender:'',
+        price:'',
+        category:'',
+        image:''
+    });
+    const handlechange=(e)=>{
+        const {name,value}=e.target;
+        console.log(name,'-',value)
+        setData({...data,[name]:value})
+    }
+    // console.log(data)
+    const Submit=()=>{
+
+    }
+    const { id, title, gender, price, category, image } = data;
+
+    return (
+        <div>
+            <form onSubmit={Submit}>
+                <input type='text' placeholder='id' name='id' value={id} onChange={handlechange} />
+                <input type='text' placeholder='title' name='title' value={title} onChange={handlechange} />
+                <input type='text' placeholder='gender' name='gender' value={gender} onChange={handlechange} />
+                <input type='text' placeholder='price' name='price' value={price} onChange={handlechange}/>
+                <input type='text' placeholder='category' name='category' value={category} onChange={handlechange} />
+                <input type='text' placeholder='image' name='image' value={image} onChange={handlechange} />
+                <input type="submit" />
+            </form>
+        </div>
+    )
+}
+export default ProductForm;
